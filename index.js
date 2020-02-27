481b5cb5fa415d0a6f9d9cad859f080607898bbc
anjaneshss01 <48996613+anjaneshss01@users.noreply.github.com>
2020-02-28 03:13:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ restService.post("/echo", function(req, res) {
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
+    req.body.queryResult.parameters.amount
+      ? req.body.queryResult.parameters.amount
       : "Seems like some problem. Speak again.";
   
   var speechResponse = {
